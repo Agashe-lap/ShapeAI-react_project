eb14784f75d7739182c795c5d1265be8663db560
Agashe-lap <86551959+Agashe-lap@users.noreply.github.com>
2021-06-27 15:36:09 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+  return (
+    <dir>
+      <header>
+        <h1>ShapeAI BootCamps</h1>
+      </header>
+    </dir>
+  );
+}
+
+export default Header;
---
+++ List.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function List() {
+  return (
+    <div>
+      <div className="note">
+        <h1>Javascript and React.js</h1>
+        <p>A basic web dev React Js Bootcamp</p>
+      </div>
+
+      <div className="note">
+        <h1>Javascript and React.js</h1>
+        <p>A basic web dev React Js Bootcamp</p>
+      </div>
+      <div className="note">
+        <h1>Javascript and React.js</h1>
+        <p>A basic web dev React Js Bootcamp</p>
+      </div>
+      <div className="note">
+        <h1>Javascript and React.js</h1>
+        <p>A basic web dev React Js Bootcamp</p>
+      </div>
+    </div>
+  );
+}
+
+export default List;
